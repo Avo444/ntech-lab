b0bd8e3015fe1624b5002e8b0da000912c77cdb7
unknown <d0cando@yandex.ru>
2022-03-28 19:21:21 +0400

    add mobile menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
 new WOW().init();
 
+$(document).ready(function() {
+    let burger = $('.burger');
+    let close = $('.close');
+
+    burger.click(function() {
+        $('.menu').addClass('active');
+        $('body').addClass('no_scroll');
+        $('.close').css('display', 'block');
+        $(this).css('display', 'none');
+    });
+    close.click(function() {
+        $('.menu').removeClass('active');
+        $('body').removeClass('no_scroll');
+        burger.css('display', 'block');
+        $(this).css('display', 'none');
+    });
+});
+
 console.log('Web-Developer: AvoPro');
